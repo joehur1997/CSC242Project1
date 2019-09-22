7ad9b5c0c0f9cbeda3dd33c45b5f79e5353113c5
cpak123123 <43483156+cpak123123@users.noreply.github.com>
2019-09-22 19:19:37 -0400

    getActions

--- src/State.java
+++ src/State.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class State {
 
 	Board state;
+	boolean whiteMove;
+	boolean blackMove;
 	String bestMoves;
 	int turn = 0;
 	int value;
@@ -16,16 +19,24 @@ public class State {
 	//minimax
 	public int isTerminal(Board state) {
 		state.score = 0;
+		if(turn%2 == 0) {
+			blackMove = true;
+			whiteMove = !blackMove;
+		}
+		else {
+			whiteMove = true;
+			blackMove = !whiteMove;
+		}
 		if(state.check(state.gameBoard)) {
-		if(state.blackwin == true && state.whitewin == false) {
-			state.score = 2;
-		}
-		if(state.blackwin == false && state.whitewin == true) {
-			state.score = -2;
-		}
-		if(state.blackwin == false && state.whitewin == false) {
-			state.score = 0;
-		}
+			if(state.blackwin == true && state.whitewin == false) {
+				state.score = 2;
+			}
+			if(state.blackwin == false && state.whitewin == true) {
+				state.score = -2;
+			}
+			if(state.blackwin == false && state.whitewin == false) {
+				state.score = 0;
+			}
 		}
 		return state.score;
 
@@ -46,6 +57,35 @@ public class State {
 
 		}
 	}
+	public String getActions(Board board, int score) {
+		ArrayList<String> bestActions = new ArrayList<String>();
+		for(Piece[] row: board.gameBoard) {
+			for(Piece piece: row) {
+				if(whiteMove) {
+					if(piece.color == 'w') {
+						for(String moves: piece.avalMoves) {
+							if(board.futureState(moves, board).score == score) {
+								bestActions.add(moves);
+							}
+						}
+					}
+				}
+				if(blackMove) {
+					if(piece.color == 'b') {
+						for(String moves: piece.avalMoves) {
+							if(board.futureState(moves, board).score == score) {
+								bestActions.add(moves);
+							}
+						}
+					}
+				}
+			}
+		}
+		Random rng = new Random();
+		int pick = rng.nextInt(bestActions.size());
+		return bestActions.get(pick);
+
+	}
 	//max and min value
 	public int maxValue(Board state) {
 		if(!state.check(state.gameBoard)) {
@@ -60,7 +100,7 @@ public class State {
 						Board result = state.futureState(moves, state);
 						v = Math.max(v, minValue(result));
 						if(minValue(result) > v) {
-							
+
 						}
 					}
 				}
@@ -92,7 +132,7 @@ public class State {
 
 	}
 	public static void main(String[] args) {
-		
+
 
 	}
 
